d509ecc1dd479161a56c416016bad5867dfefe8c
Yeudakimenka-Maksim <evdokimenko.maxim@gmail.com>
2016-07-25 23:50:11 +0300

    Completed task

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using PersonLibrary;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person { FirstName = "Alex", LastName = "Ivanov" };
+            var p2 = new Person { FirstName = "Alex", LastName = "Ivanov" };
+
+            var equalPeople = p1.Equals(p2); // Uses Person.Equals
+
+            // Use Person.GetHashCode and Person.Equals on add
+            var people1 = new HashSet<Person> { p1, p2, new Person { FirstName = "Maksim", LastName = "Petrov" } };
+            var people2 = new HashSet<Person> { new Person { FirstName = "Dmitriy", LastName = "Sokolov" }, p2 };
+
+            people1.ToList().Sort(); // Uses Person.CompareTo
+        }
+    }
+}
---
+++ PersonLibrary/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace PersonLibrary
+{
+    public class Person : IEquatable<Person>, IComparable<Person>
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool Equals(Person other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return string.Equals(this.FirstName, other.FirstName) &&
+                   string.Equals(this.LastName, other.LastName);
+        }
+
+        public int CompareTo(Person other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return 0;
+            }
+
+            var comparison = string.CompareOrdinal(this.LastName, other.LastName);
+
+            return comparison != 0 ? comparison : string.CompareOrdinal(this.FirstName, other.FirstName);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((this.FirstName?.GetHashCode() ?? 0) * 397) ^
+                       (this.LastName?.GetHashCode() ?? 0);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}";
+        }
+    }
+}
